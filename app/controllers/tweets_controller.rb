85e302319c6b26ca721b3e338421055b8dc9d8d4
EC2 Default User <ec2-user@ip-172-31-38-195.ap-southeast-1.compute.internal>
2018-12-02 09:09:54 +0000

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,16 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+  
+  def new
+  end
+  
+  def create
+    Tweet.create(tweet_params)
+  end
+  
+  def tweet_params
+    params.permit(:text)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'tweets#index'
+  get 'tweets/new' => 'tweets#new'
+  post 'tweets' => 'tweets#create'
+  # get 'tweets' => 'tweets#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
